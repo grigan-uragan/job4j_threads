2f6b4b8c01254bc61134bead8b48342f7924ca71
grigan-Uragan <prischepny@mail.ru>
2021-02-13 00:35:12 +0300

    2. Обеспечить остановку потребителя. [#469400]

---
+++ src/main/java/ru/job4j/concurrent/ParallelSearch.java
@@ -0,0 +1,35 @@
+package ru.job4j.concurrent;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread consumer = new Thread(
+                () -> {
+                    while (true) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        , "Consumer");
+        consumer.setDaemon(true);
+        consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < 3; i++) {
+                        queue.offer(i);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        , "Producer");
+        producer.start();
+        try {
+            producer.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+    }
+}
