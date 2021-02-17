001762042ee13230320fe48a2532bf0c8c288039
grigan-Uragan <prischepny@mail.ru>
2021-02-17 16:21:22 +0300

    2. Thread switcher [#469431]

--- src/main/java/ru/job4j/concurrent/MasterSlaveBarrier.java
+++ src/main/java/ru/job4j/concurrent/MasterSlaveBarrier.java
@@ -1,6 +1,11 @@
 package ru.job4j.concurrent;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class MasterSlaveBarrier {
+    @GuardedBy("this")
     private boolean flag = false;
 
     public void tryMaster() throws InterruptedException {
